599d2ef8e74881dd453f3d512c63315a7250aeb3
mian-mamoon-shah <157153473+mian-mamoon-shah@users.noreply.github.com>
2024-02-12 20:56:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// let num2 =("please inter the first num")
+
+// let num1=("please inter the second num")
+// alert(res);
+let firstname = prompt('please enter your first name');
+let lastname = prompt('please enter your last name')
+alert(res)
